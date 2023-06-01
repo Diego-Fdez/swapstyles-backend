c54ddcd3be1b438cd3dcae5caadb0750b1586439
Diego-Fdez <dieghoxcr7@gmail.com>
2023-06-01 13:29:17 -0600

    create product interface

---
+++ src/interfaces/products.interface.ts
@@ -0,0 +1,15 @@
+export interface ProductInterface {
+  userId: number;
+  category: string;
+  title: string;
+  description: string;
+  size: string;
+  condition: string;
+  range: number;
+  brand: string;
+  price: number;
+  quantity: number;
+  color: string;
+  isPremium: boolean;
+  status: boolean;
+}
