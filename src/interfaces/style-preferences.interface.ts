f225c2f58425fbf88326e683147af095732f04bf
Diego-Fdez <dieghoxcr7@gmail.com>
2023-06-01 17:27:23 -0600

    create style preferences interface

---
+++ src/interfaces/style-preferences.interface.ts
@@ -0,0 +1,9 @@
+export interface StylePreferencesInterface {
+  userId: number;
+  preference1: string;
+  preference2: string;
+  preference3: string;
+  preference4: string;
+  preference5: string;
+  preference6: string;
+}
