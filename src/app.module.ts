9f6ec4f993620ca65ab60686c32fef05945627b9
Diego-Fdez <dieghoxcr7@gmail.com>
2023-06-01 13:28:20 -0600

    add products module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSourceConfig } from './config/data.source';
 import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { UsersModule } from './users/users.module';
     }),
     TypeOrmModule.forRoot({ ...DataSourceConfig }),
     UsersModule,
+    ProductsModule,
   ],
 })
 export class AppModule {}
