9f274877c9c413904507fc6722da0920ca4b49b1
Diego-Fdez <dieghoxcr7@gmail.com>
2023-06-01 13:30:16 -0600

    create product dto

---
+++ src/products/dto/product.dto.ts
@@ -0,0 +1,129 @@
+import {
+  IsBoolean,
+  IsEmpty,
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  MaxLength,
+} from 'class-validator';
+import { Categories } from 'src/constants';
+
+export class CreateProductDTO {
+  @IsNotEmpty()
+  @IsNumber()
+  userId: number;
+
+  @IsEnum(Categories)
+  @IsNotEmpty()
+  category: Categories;
+
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(75)
+  title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(350)
+  description: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(2)
+  size: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(350)
+  condition: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  range: number;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(75)
+  brand: string;
+
+  @IsOptional()
+  @IsNumber()
+  price: number;
+
+  @IsOptional()
+  @IsNumber()
+  quantity: number;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(40)
+  color: string;
+
+  @IsOptional()
+  @IsEmpty({ message: 'The field must have no value when it is sent as default.' })
+  isPremium: boolean;
+
+  @IsOptional()
+  @IsEmpty({ message: 'The field must have no value when it is sent as default.' })
+  status: boolean;
+}
+
+export class UpdateProductDTO {
+  @IsEmpty({ message: 'The field userId must have no value when it is sent as default.' })
+  userId: number;
+
+  @IsEnum(Categories)
+  @IsOptional()
+  category: Categories;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(75)
+  title: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(350)
+  description: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(2)
+  size: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(350)
+  condition: string;
+
+  @IsOptional()
+  @IsNumber()
+  range: number;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(75)
+  brand: string;
+
+  @IsOptional()
+  @IsNumber()
+  price: number;
+
+  @IsOptional()
+  @IsNumber()
+  quantity: number;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(40)
+  color: string;
+
+  @IsEmpty({ message: 'The field isPremium must have no value when it is sent as default.' })
+  isPremium: boolean;
+
+  @IsOptional()
+  @IsBoolean()
+  status: boolean;
+}
