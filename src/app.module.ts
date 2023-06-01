7dada3987acf64d2ecf8072637266812a7893f59
Diego-Fdez <dieghoxcr7@gmail.com>
2023-06-01 17:27:07 -0600

    add stylePreference module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSourceConfig } from './config/data.source';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
+import { StylePreferencesModule } from './style-preferences/style-preferences.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { ProductsModule } from './products/products.module';
     TypeOrmModule.forRoot({ ...DataSourceConfig }),
     UsersModule,
     ProductsModule,
+    StylePreferencesModule,
   ],
 })
 export class AppModule {}
