7078df189d1abaf65f0a90860787027f46097698
Diego-Fdez <dieghoxcr7@gmail.com>
2023-06-02 11:04:07 -0600

    validate if the user has make 2 publications in the current month

--- src/products/services/products.service.ts
+++ src/products/services/products.service.ts
@@ -25,9 +25,41 @@ export class ProductsService {
       });
 
       //if the user does not exist, throw an error
-      if (!userExist) {
-        throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+      if (!userExist) throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+
+      //if the user exists and !isPremium
+      if (!userExist.isPremium) {
+        //check if the user has made 2 products in the same month
+        const last2Insertions = await this.productRepository.query(
+          `SELECT created_at FROM products WHERE user_id = ${body.userId} ORDER BY created_at DESC LIMIT 2`,
+        );
+
+        //You get the last 2 dates
+        const lastInsertionDate = last2Insertions[0]?.created_at;
+        const secondLastInsertionDate = last2Insertions[1]?.created_at;
+
+        //if dates are not null
+        if (lastInsertionDate & secondLastInsertionDate) {
+          //check if the 2 dates are in the same month
+          const isSameMonth = lastInsertionDate.getMonth() === secondLastInsertionDate.getMonth();
+
+          if (isSameMonth) {
+            //calculate the number of days until the next month
+            const currentDate = new Date();
+            const endOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
+            const daysUntilNextMonth = endOfMonth.getDate() - currentDate.getDate();
+
+            //if the user has made 2 products in the same month, throw an error
+            throw new HttpException(
+              `You are not yet a premium user, you must wait ${daysUntilNextMonth} days to make your next publication`,
+              HttpStatus.BAD_REQUEST,
+            );
+          }
+        }
       }
+
+      //if the user exists and isPremium
+      if (userExist.isPremium) body.isPremium = true;
 
       //if the user exists, create a new product
       return await this.productRepository.save(body);
